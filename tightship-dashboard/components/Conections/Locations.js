d5c986a281acc95ce699c7537cdd6396476c3679
Paul Mikulskis <pmikulskis@logrocket.com>
2021-11-30 09:58:56 -0500

    locations geocoding

--- tightship-dashboard/components/Conections/Locations.js
+++ tightship-dashboard/components/Conections/Locations.js
@@ -8,10 +8,10 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import ShareIcon from '@mui/icons-material/Share';
+import AddBusinessIcon from '@mui/icons-material/AddBusiness';
 import Divider from '@mui/material/Divider';
-import Autocomplete from '@mui/material/Autocomplete';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 import TextField from '@mui/material/TextField';
 import Chip from '@mui/material/Chip';
 import FormGroup from '@mui/material/FormGroup';
@@ -25,7 +25,9 @@ import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
 import Backdrop from '@mui/material/Backdrop';
 import Typography from '@mui/material/Typography';
+import Geocode from "react-geocode";
 
+import axios from 'axios';
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 import LocationsTable from './LocationsTable';
 
@@ -45,11 +47,25 @@ const StyledLocationsTable = styled(Box)`
     padding-top: 4rem;
 `;
 
+const StyledAddNew = styled(Box)`
+    margin: 1rem 1rem 0 1rem;
+    padding: 1rem 20%;
+`;
+
 const Locations = () => {
 
     const db = getFirestore();
     const user = useFirebaseAuth();
-    const [locations, setLocations] = useState({})
+    const [locations, setLocations] = useState({});
+    const [newName, setNewName] = useState(null);
+    const [newCategory, setNewCategory] = useState(null);
+    const [newPlace, setNewPlace] = useState(null);
+
+    const formReady = (
+        (newPlace != null) &&
+        (newCategory != null) &&
+        (newName != null && newName.length > 3)
+    );
 
     const getLocations = async () => {
         const saveResult = await getDoc(doc(db, 'locations', user.uid));
@@ -57,16 +73,100 @@ const Locations = () => {
         return true;
     };
 
+    console.log(JSON.stringify(newPlace, null, 3))
     useEffect( () => {
         getLocations();
-    }, [])
+    }, []);
+
+    const selectChange = (e) => {
+        setNewCategory(e.target.value);
+    };
+
+    const saveNewLocation = async () => {
+        const url=`https://maps.googleapis.com/maps/api/geocode/json?place_id=${newPlace.value.place_id}&key=${process.env.FIREBASE_API_KEY}`
+        await axios({
+            method: 'get',
+            url: url,
+        }).then(result => {
+            const firstPick = result.data.results.pop();
+            console.log(JSON.stringify(firstPick, null, 3))
+            const { lat, lng } = firstPick.geometry.location;
+            const streetNum = firstPick.address_components.filter(c => c.types.includes('street_number')).pop()?.short_name || '';
+            const streetName = firstPick.address_components.filter(c => c.types.includes('route')).pop()?.short_name || '';
+            const locality = firstPick.address_components.filter(c => c.types.includes('locality')).pop()?.short_name || '';
+            const zip = firstPick.address_components.filter(c => c.types.includes('postal_code')).pop()?.short_name || '';
+            const data = { [newName]: {
+                name: newName,
+                category: newCategory,
+                placeId: newPlace.value.place_id,
+                lat: lat,
+                lon: lng,
+                address: `${streetNum} ${streetName}`,
+                locality: locality,
+                zip: zip,
+            }};
+            setDoc(
+                doc(db, 'locations', user.uid),
+                data,
+                { merge: true }
+            ).then(() => {
+                setLocations({...locations, ...data})
+            })
+        });
+    };
 
 
     return (
         <LocationsContainer>
             <GooglePlacesAutocomplete
+                selectProps={{
+                    newPlace,
+                    onChange: setNewPlace,
+                }}
                 apiKey={process.env.FIREBASE_API_KEY}
             />
+            <StyledAddNew>
+                <Stack direction='row' spacing={1}>
+                    <TextField
+                        color='secondary'
+                        sx={{
+                            width: '100%',
+                        }}
+                        label='new place nickname'
+                        variant={'standard'}
+                        value={newName}
+                        onChange={(e) => setNewName(e.target.value)}
+                    />
+                    <Select
+                        color='secondary'
+                        sx={{
+                            width: '100%'
+                        }}
+                        value={newCategory}
+                        label="Category"
+                        onChange={selectChange}
+                        >
+                        <MenuItem value={'personal'}>personal</MenuItem>
+                        <MenuItem value={'vaulter'}>vaulter</MenuItem>
+                        <MenuItem value={'business'}>business</MenuItem>
+                        <MenuItem value={'bank'}>bank</MenuItem>
+                    </Select>
+                    <Button
+                        variant={formReady ? 'outlined' : 'disabled'}
+                        color='secondary'
+                        sx={{
+                            width: '100%',
+                        }}
+                        onClick={() => saveNewLocation()}
+                    >
+                        <Stack direction='column' alignItems='center' spacing={0}>
+                            <AddBusinessIcon />
+                            <Typography sx={{fontSize: '7pt'}} variant="caption">add location</Typography>
+                        </Stack>
+                    </Button>
+                </Stack>
+
+            </StyledAddNew>
             <StyledLocationsTable>
                 <LocationsTable locations={[locations, setLocations]}/>
             </StyledLocationsTable>
